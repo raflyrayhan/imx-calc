9a26f02dfe2c0a66241f7a50e34982e72370f09b
unknown <raflyray00@gmail.com>
2025-08-18 16:24:25 +0700

    rules change

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'http://192.168.56.1:3000, http://localhost:3000',
-          },
-        ],
-      },
-    ];
+ eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
